6622466cf885f30b8d9ca284374fead5a770d60b
hanul-cha <ckgks3014@naver.com>
2021-07-19 18:32:25 +0900

    first commit

---
+++ src/component/Hello.js
@@ -0,0 +1,36 @@
+const Hello = () => {
+    function showName() {
+        console.log("mike");
+    }
+    function showAge(age) {
+        console.log(age);
+    }
+    function showText(txt) {
+        console.log(txt);
+    }
+
+
+    return (
+        <div>
+            <h1>Hello</h1>
+            <button onClick={showName}>Show name</button>
+            <button
+                onClick={() => {
+                    showAge(10);
+                }}
+            >
+                Show age
+            </button>
+            <input
+                type="text"
+                onChange={e => {
+                    const txt = e.target.value;
+                    showText(txt);
+                }}
+            />
+        </div>
+        
+    );    
+};
+
+export default Hello;
