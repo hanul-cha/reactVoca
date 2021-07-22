23bc0bb6c1e57029d0158f899ae20812d51a9a61
hanul-cha <ckgks3014@naver.com>
2021-07-22 17:26:09 +0900

    link data

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,13 +1,24 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import dummy from '../db/data.json';
 import Word from './Word.js';
 
 
 export default function Day() {
 
     const day =useParams().day;
-    const wordList = dummy.words.filter(word => (word.day === Number(day)))
-    
+    /* const wordList = dummy.words.filter(word => (word.day === Number(day))) */
+    const [words, setWords] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setWords(data);
+               
+            })
+    }, [])
     
 
    return (
@@ -15,7 +26,7 @@ export default function Day() {
     <h2>Day {day}</h2>
     <table>
         <tbody>
-            {wordList.map(word => (
+            {words.map(word => (
                <Word word={word} key={word.id}/>
             ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,14 +1,35 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import dummy from '../db/data.json';
 
 export default function DayList() {
-    console.log(dummy);
-    return <ul className="list_day">
-        {dummy.days.map(day => (
-            <li key={day.id}>
-                <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-                
-            </li>
-        ))}
-    </ul>;
+    const [days, setDays] = useState([]);
+
+    
+    useEffect(() => {
+        fetch("http://localhost:3001/days")
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setDays(data);
+                console.log(data);
+               
+            })
+    }, []);//매게변수([]빈배열을 사용하면 랜더링 직수 1회만)가 변경될때만 실행한다 - 의존성배열이라고함 
+
+
+    return (
+        <>
+            <ul className="list_day">
+                {days.map(day => (
+                    <li key={day.id}>
+                        <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+                        
+                    </li>
+                ))}
+            </ul>
+            
+        </>
+    )
 }
+
